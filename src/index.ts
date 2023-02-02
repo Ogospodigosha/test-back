57b997dfd24c028915e9545d2c4c7b0b4f9308b9
Ogospodigosha <odintsovis@hotmail.com>
2023-02-03 02:13:35 +0300

    add(products): delete product point

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,29 @@ import express, {Request, Response}  from 'express'
 const app = express()
 const port = 3000
 
-const products = [{title: 'tomato'}, {title: 'apple'}]
+const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'apple'}]
 const addresses = [{id: 1, value: 'Nachimova'}, {id: 2, value: 'Molodejnaya'}]
 
 app.get('/products', (req: Request, res: Response) => {
     res.send(products)
 })
-app.get('/products/:productTitle', (req: Request, res: Response) => {
-    let product = products.find(el => el.title === req.params.productTitle)
+app.get('/products/:id', (req: Request, res: Response) => {
+    let product = products.find(el => el.id === +req.params.id)
     if (product) {
         res.send(product)
     } else {
         res.send(404)
     }
-
+})
+app.delete('/products/:id', (req: Request, res: Response) => {
+    for (let i=0; i<products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i,1)
+            res.send(204)
+            return
+        }
+    }
+    res.send(404)
 })
 app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses)
